7820d9e9783be9eca1dde323a45fb91593f8f297
Samuel Dobbie <samueljdobbie@gmail.com>
2019-02-22 12:40:34 +0000

    Add ability to display read in data and annotation configurations

--- annotate/urls.py
+++ annotate/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^annotate/(?P<file_path>[\s\S]+)/$', views.annotate_data, name='annotate_data'),
+    url(r'^annotate/(?P<data_file_path>[\s\S]+)/$', views.annotate_data, name='annotate_data'),
 ]
--- annotate/views.py
+++ annotate/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render
 import os
 
-def annotate_data(request, file_path):
+def annotate_data(request, data_file_path):
+    data = dict()
+
     os.chdir('/')
-    data = open(file_path, encoding='utf8').read()
-    return render(request, 'file_selection/index.html', {})
+
+    file_data = get_file_lines(data_file_path)
+    data['file_data'] = file_data
+
+    config_file_path = os.path.dirname(data_file_path) + '/annotation.conf'
+    config_data = get_file_lines(config_file_path)
+    config_data = [x.strip() for x in config_data]
+
+    configs = []
+    config_key = ''
+    config_values = []
+    for line in config_data:
+        if line == '':
+            continue
+        
+        if len(line) >= 3 and line[:3] == '###':
+            if config_key != '':
+                configs.append(config_key)
+                data[config_key] = config_values
+                config_values = []
+            config_key = line[4:]
+            continue
+
+        config_values.append(line)
+
+    if config_values != []:
+        configs.append(config_key)
+        data[config_key] = config_values
+
+    context = dict()
+    context['dict'] = data
+
+    return render(request, 'annotate/annotate.html', context)
+
+
+def get_file_lines(file_path):
+    with open(file_path, encoding='utf8') as f:
+        return f.readlines()
