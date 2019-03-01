a3271f3d50d3788e7ec1978a7c4ee9786ad85b34
Samuel Dobbie <samueljdobbie@gmail.com>
2019-03-01 12:55:21 +0000

    Add dark mode

--- annotate/static/annotate/annotate.js
+++ annotate/static/annotate/annotate.js
@@ -9,11 +9,13 @@ $(document).ready(function () {
     var allAnnotations = [];
     var offsets = [];
     var stopwordsHidden = false;
-    var stopwords = ["therefore", "see", "able", "yet", "know", "get", "saw", "known", "perhaps", "might", "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"];
+    var stopwords = ["therefore", "today", "see", "able", "yet", "know", "get", "saw", "known", "perhaps", "might", "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"];
 
 
     $('#addAnnotation').click(function () {
         var highlighted = window.getSelection().toString();
+        var div = document.getElementById('file_data');
+        var sel = getSelection();
 
         // Check whether selected text is valid
         var validAnnotation = false;
@@ -25,27 +27,16 @@ $(document).ready(function () {
             }
         }
 
-        if (!validAnnotation || highlighted == '') {
+        if (!validAnnotation || highlighted == '' || sel.anchorNode.nodeName == 'DIV' || !sel) {
             return;
         }
 
         // Get start and end indicies (offsets) of selected text
-        var div = document.getElementById('file_data');
-        var text = div.innerText;
-
-        var sel = getSelection();
         var result = { start: null, end: null };
-
-        if (sel.anchorNode.nodeName == 'DIV') {
-            return;
-        }
-
         ['start', 'end'].forEach(which => {
             var counter = 1;
             var tmpNode = div.querySelector('span');
             var node = which == 'start' ? 'anchor' : 'focus';
-
-            if (!sel) return;
 
             while (tmpNode != sel[node + 'Node'].parentElement) {
                 if (tmpNode != null) {
@@ -66,14 +57,15 @@ $(document).ready(function () {
             endIndex = result.start;
         };
 
-        //Add annotaion to current-annotataion list
-        var annotation = [];
         // Color-highlight selected text
         document.getElementById('file_data').contentEditable = 'true';
         document.execCommand('backColor', false, '#33FFB5');
+        document.execCommand('foreColor', false, 'black');
         document.getElementById('file_data').contentEditable = 'false';
+        
 
         // Format annotation(s) to be in stand-off format
+        var annotation = [];
         for (k in dict) {
             if (k != 'file_data' && k != 'ann_filename') {
                 if (document.querySelector('input[name=' + k + ']:checked') != null) {
@@ -92,16 +84,30 @@ $(document).ready(function () {
                         id = "E" + eventIndex;
                         eventIndex++;
                     }
+
+                    // TO-Do: Implement correct stand-off formatting
+
                     annotation.push([id + '\t' + document.querySelector('input[name=' + k + ']:checked').value + " " + startIndex + " " + endIndex + "\t" + highlighted + '\n']);
                 }
             }
         }
         // Keep track of offets for each annotation
         offsets.push([startIndex, endIndex]);
+
+        //Add annotaion to current-annotataion list
         allAnnotations.push(annotation);
 
         // Add annotation to annotaion_data display
-        document.getElementById('annotation_data').innerHTML += '<p class="test" id="' + startIndex + '_' + endIndex + '" style="background-color:#33FFB5; font-family:\'Nunito\'; padding:10px; border:2px solid #888; display:inline-block; clear:both; float:left;">' + highlighted + '</p>';
+        var annotationClass = 'class="test"';
+        var annotationId = 'id="' + startIndex + '_' + endIndex + '"';
+        var annotationStyle = 'style="border-radius:5px; background-color:#33FFB5; font-family:\'Nunito\'; padding:10px; border:2px solid #333; display:inline-block; clear:both; float:left; '
+        if (darkMode) {
+            annotationStyle += 'color:black;"';
+        } else {
+            annotationStyle += '"'
+        }
+            document.getElementById('annotation_data').innerHTML += '<p ' + annotationClass + ' ' + annotationId + ' ' + annotationStyle + '>' + highlighted + '</p>';
+        
         // Un-highlights newly-annotated text
         window.getSelection().removeAllRanges();
     });
@@ -154,22 +160,74 @@ $(document).ready(function () {
         document.getElementById("downloadButton").href = textFile;
     });
 
+
     // Delete clicked annotation
     $('#annotation_data').on('click', '.test', function (event) {
         var indicies = event.target.id.split("_");
         var startIndex = indicies[0];
         var endIndex = indicies[1];
 
+        document.getElementById('file_data').contentEditable = 'true';
+        document.execCommand('RemoveFormat', false, null);
+        document.getElementById('file_data').contentEditable = 'false';
+
+        // Finds correct annotation index based on offset list and removes
         for (var i = 0; i < offsets.length; i++) {
             if (offsets[i][0] == startIndex && offsets[i][1] == endIndex) {
                 allAnnotations.splice(i, 1);
                 offsets.splice(i, 1);
+
+                // Removes annotation from annotation_data display
                 var elem = document.getElementById(event.target.id);
                 elem.parentElement.removeChild(elem);
+
+                // Remove highlight from annotation
+
                 return;
             }
         };
     });
 
-    
+
+    // Display information about annotation on hover
+    $("#annotation_data").mouseover(function(eventObj) {
+        var id = eventObj.target.id;
+        if (id != 'annotation_data') {
+            document.getElementById(id).title = "Entity: None\nAttributes: Negation\n";
+        } 
+    });
+
+
+    // Prevent deselection of text when deleting annotation
+    $('#annotation_data').mousedown(function() {
+        return false;
+    });
+
+
+    // Prevent deselection of text when selecting configuration
+    $('#config_data').mousedown(function() {
+        return false;
+    });
+
+    var darkMode = false;
+    var backgroundColor = '';
+    var textColor = '';
+    // Allows users to switch to darkm mode
+    $('#darkMode').click(function () {
+        if (!darkMode) {
+            backgroundColor = '#333';
+            textColor = 'rgb(210, 210, 210)';
+            darkMode = true;
+        } else {
+            backgroundColor = 'white';
+            textColor = 'black';
+            darkMode = false;
+        }
+
+        $('body').css({
+            "background-color": backgroundColor,
+            "color" : textColor
+        });
+    });
+
 });
