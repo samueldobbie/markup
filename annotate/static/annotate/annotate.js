b39f6bdfca315b880323bc7c560f99a120325d91
Samuel Dobbie <samueljdobbie@gmail.com>
2019-03-02 01:29:01 +0000

    Allow for proper annotation deletion (removes highlighting from file_data text)

--- annotate/static/annotate/annotate.js
+++ annotate/static/annotate/annotate.js
@@ -1,7 +1,6 @@
 
 $(document).ready(function () {
 
-
     var entityIndex = 1;
     var relationIndex = 1;
     var eventIndex = 1;
@@ -12,7 +11,18 @@ $(document).ready(function () {
     $('#addAnnotation').click(function () {
         var highlighted = window.getSelection().toString();
         
-        if (highlighted == '') { return; };
+        // Check whether selected text is valid
+        var validAnnotation = false;
+        for (k in dict) {
+            if (k != 'file_data' && k != 'ann_filename') {
+                if (document.querySelector('input[name=' + k + ']:checked') != null) {
+                    validAnnotation = true;
+                    break;
+                }
+            }
+        }
+
+        if (!validAnnotation || highlighted == '') { return; }
         
         // Get start and end indicies (offsets) of selected text
         var doc = document.getElementById('file_data');
@@ -27,8 +37,7 @@ $(document).ready(function () {
 
         // Color-highlight selected text
         document.getElementById('file_data').contentEditable = 'true';
-        document.execCommand('backColor', false, '#33FFB5');
-        document.execCommand('foreColor', false, 'black');
+        document.execCommand('insertHTML', false, '<span class="' + startIndex + '_' + endIndex + '" style="background-color:#33FFB5; color:black;">' + highlighted + '</span>');
         document.getElementById('file_data').contentEditable = 'false';        
 
         var ent_hover_info = [];
@@ -60,6 +69,8 @@ $(document).ready(function () {
                         id = "E" + eventIndex;
                         eve_hover_info.push(annotationValue);
                         eventIndex++;
+                    } else {
+                        continue;
                     }
 
                     // TO-Do: Implement correct stand-off formatting
@@ -109,41 +120,15 @@ $(document).ready(function () {
     });
 
 
-    /*
-    // Add ability to hide or show stopwords
-    //var stopwordsHidden = false;
-    //var stopwords = [deleted];
-    $('#hideStopwords').click(function () {
-        var wordColor = '';
-        if (!stopwordsHidden) {
-            wordColor = '#efeff5';
-            document.getElementById('hideStopwords').innerText = "Show Stopwords";
-            stopwordsHidden = true;
-        } else {
-            wordColor = 'black';
-            document.getElementById('hideStopwords').innerText = "Hide Stopwords";
-            stopwordsHidden = false;
-        }
-
-        for (var i = 0; i < stopwords.length; i++) {
-            var myElements = document.querySelectorAll("." + stopwords[i]);
-            for (var j = 0; j < myElements.length; j++) {
-                myElements[j].style.color = wordColor;
-            }
-        }
-    });
-    */
-
-
     // Delete clicked annotation
     $('#annotation_data').on('click', '.test', function (event) {
-        var indicies = event.target.id.split("_");
+        var id = event.target.id;
+        var indicies = id.split("_");
         var startIndex = indicies[0];
         var endIndex = indicies[1];
 
-        document.getElementById('file_data').contentEditable = 'true';
-        document.execCommand('RemoveFormat', false, null);
-        document.getElementById('file_data').contentEditable = 'false';
+        // Remove span tag from file_data text
+        document.getElementsByClassName(id)[0].outerHTML = document.getElementsByClassName(id)[0].innerHTML;
 
         // Finds correct annotation index based on offset list and removes
         for (var i = 0; i < offsets.length; i++) {
@@ -152,10 +137,8 @@ $(document).ready(function () {
                 offsets.splice(i, 1);
 
                 // Removes annotation from annotation_data display
-                var elem = document.getElementById(event.target.id);
+                var elem = document.getElementById(id);
                 elem.parentElement.removeChild(elem);
-
-                // Remove highlight from annotation
 
                 return;
             }
@@ -198,11 +181,13 @@ $(document).ready(function () {
         return false;
     });
 
+
     var darkMode = false;
-    var backgroundColor = '';
-    var textColor = '';
     // Allows users to switch to darkm mode
     $('#darkMode').click(function () {
+        var backgroundColor = '';
+        var textColor = '';
+
         if (!darkMode) {
             backgroundColor = '#333';
             textColor = 'rgb(210, 210, 210)';
@@ -212,10 +197,12 @@ $(document).ready(function () {
             textColor = 'black';
             darkMode = false;
         }
+
         // To-do: Deal with text coloring issue when annotating then switching
         $('body').css({
             "background-color": backgroundColor,
             "color" : textColor
         });
+
     });
 });
