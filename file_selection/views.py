6ff2c43bc1e2e8416ac774376fba29489ba9cc6b
Samuel Dobbie <samueljdobbie@gmail.com>
2019-03-08 15:57:22 +0000

    Update homepage + add navbar to each page

--- file_selection/views.py
+++ file_selection/views.py
@@ -1,23 +1,18 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from tkinter import filedialog
 import tkinter as tk
+from tkinter import filedialog
+import time
 
 def file_selection_template(request):
     if request.method == 'GET':
         if request.GET.get('open_file_button'):
             root = tk.Tk()
             root.withdraw()
-            file_path = str(filedialog.askopenfilename())
+            # TO-DO: Fix "main thread is not in main loop" error
+            file_path = filedialog.askopenfilename()
             if file_path != '()' and len(file_path) != 0:
                 return redirect('/annotate' + file_path)
             else:
                 return redirect('/')
     return render(request, 'file_selection/index.html', {})
-
-
-def open_file(request):
-    root = tk.Tk()
-    root.withdraw()
-    return str(filedialog.askopenfilename())
-
