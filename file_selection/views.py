ea12462bc225cedaa6be8b28b9b6a840b8719755
Samuel Dobbie <samueljdobbie@gmail.com>
2019-08-03 12:59:34 +0100

    Fix file dialog issue

--- file_selection/views.py
+++ file_selection/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from annotate import views as annotate_views
-import easygui
+import PySimpleGUI as gui
 
 def file_selection_template(request):
     # Reset currently opened file lists
@@ -13,14 +13,14 @@ def file_selection_template(request):
     file_path = ''
     if request.method == 'GET':
         if request.GET.get('open_file_button'):
-            file_path = easygui.fileopenbox()
-            if file_path != None:
+            file_path = gui.PopupGetFile("Choose a file", no_window=True)
+            if file_path != None and file_path != ():
                 return redirect('/annotate' + file_path)
             else:
                 return redirect('/')
         elif request.GET.get('open_dir_button'):
-            dir_path = easygui.diropenbox()
-            if dir_path != None:
+            dir_path = gui.PopupGetFolder("Choose a folder", no_window=True)
+            if dir_path != None and dir_path != ():
                 return redirect('/annotate' + dir_path)
             else:
                 return redirect('/')
