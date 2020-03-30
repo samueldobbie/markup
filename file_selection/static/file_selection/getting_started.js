35621b37da125cf3fd146899757a356c622030b2
Samuel <NA>
2020-03-30 23:16:35 +0100

    Add base templates to build upon

--- file_selection/static/file_selection/getting_started.js
+++ file_selection/static/file_selection/getting_started.js
@@ -256,10 +256,66 @@ function sleep(time) {
 }
 
 
+// Function to GET csrftoken from Cookie
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+
+// Function to set Request Header with `CSRFTOKEN`
+function setRequestHeader(csrftoken){
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+}
+
+
 function storeFileDataLocally(file, localStorageName) {
-    var reader = new FileReader();
-    reader.onload = function () {
-        localStorage.setItem(localStorageName, reader.result);
-    };
-    reader.readAsText(file);
+    setRequestHeader(getCookie('csrftoken'));
+
+    if (file.type == 'application/pdf') {
+        var fileReader = new FileReader();
+        fileReader.onload = function() {
+
+            //Step 4:turn array buffer into typed array
+            var typedarray = new Uint8Array(this.result);
+    
+            //Step 5:PDFJS should be able to read this
+            PDFJS.getDocument(typedarray).then(function(pdf) {
+                console.log(pdf);
+            });
+    
+    
+        };
+        //Step 3:Read the file as ArrayBuffer
+        fileReader.readAsArrayBuffer(file);
+    } else {
+        var reader = new FileReader();
+        reader.onload = function () {
+            localStorage.setItem(localStorageName, reader.result);
+        };
+        reader.readAsText(file);
+    }
 }
--- file_selection/views.py
+++ file_selection/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def create_config(request):
