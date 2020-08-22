f1d1b62c0f8c6ab12dd78f6232f493d2383f5a7c
Samuel Dobbie <samuel.jordan.dobbie@gmail.com>
2020-08-22 19:15:05 +0100

    Fix serving of static files in production

--- markup/settings.py
+++ markup/settings.py
@@ -145,7 +145,12 @@ STATIC_URL = '/static/'
 
 if DEBUG:
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
+        os.path.join(BASE_DIR,'static')
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR,'static')
+    STATICFILES_DIRS = [
+        'D:/home/site/wwwroot/static',
+        os.path.join(BASE_DIR,'static')
+    ]
+
--- markup/urls.py
+++ markup/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import include, path, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('file_selection.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
