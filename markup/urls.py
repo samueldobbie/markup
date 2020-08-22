5f3b2a1e2abc55e76140c053b94bb01b3993226e
Samuel Dobbie <samuel.jordan.dobbie@gmail.com>
2020-08-22 15:34:44 +0100

    Include static url pattern

--- markup/urls.py
+++ markup/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('file_selection.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
