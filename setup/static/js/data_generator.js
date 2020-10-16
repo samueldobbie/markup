d1a2ede5362fcf5e4e4878beac0f099400353b7f
Samuel Dobbie <samuel.jordan.dobbie@gmail.com>
2020-10-16 13:01:46 +0100

    Refactor data generator

--- setup/static/js/data_generator.js
+++ setup/static/js/data_generator.js
@@ -1,10 +1,6 @@
-$(document).ready(function () {    
+$(document).ready(function () {
     $('#variable-dropdown').change(function () {
-        /*
-        Display variable fields based on
-        dropdown selection
-        */
-       
+        // Show fields based on dropdown selection
         if ($(this).val() == 'text') {
             $('#variable-text-value').show();
             $('#variable-numerical-range').hide();
@@ -14,208 +10,73 @@ $(document).ready(function () {
         }
     });
 
-
-    $('#add-variable').click(function () {
-        /*
-        Add variable for use in sentence
-        template during training data generation
-        */
-
-        var variable = {};
-        variable['type'] = $('#variable-dropdown').val();
-        variable['name'] = $('#variable-name').val().toLowerCase().trim();
-
-        // Check that valid name has been entered
-        if (variable['name'] == '') {
-            alert('Variable name cannot be empty.');
-            return;
-        }
-
-        // Check that variable name hasn't already been used
-        if (addedVariableNames.includes(variable['name'])) {
-            alert('Variable name already exists!');
-            return;
-        }
-
-
-        // Parse input values
-        variable['values'] = [];
-        if (variable['type'] == 'text') {
-            // Tokenise and clean input values
-            var values = $('#variable-text-value').val().split(',');
-            for (valueIndex in values) {
-                var value = values[valueIndex].trim();
-                if (value != '') {
-                    variable['values'].push(values[valueIndex].trim());
-                }
-            }
-            
-            if (variable['values'].length == 0) {
-                alert('Values cannot be empty.');
-                return;
-            }
-        } else {
-            var low = parseInt($('#numerical-range-low').val());
-            var high = parseInt($('#numerical-range-high').val());
-            var step = parseInt($('#numerical-range-step').val());
-
-            // Check that all numerical fields have been populated
-            if (isNaN(low) || isNaN(high) || isNaN(step)) {
-                alert('Numerical range fields cannot be empty.');
-                return;
-            }
-
-            // Check that valid range has been entered
-            if (low > high) {
-                alert('Low value cannot be greater than the high value.');
-                return;
-            }
-
-            // Generate range values
-            for (var i = low; i <= high; i += step) {
-                variable['values'].push(i);
-            }
-        }
-
-        // Clear input fields
-        $('#variable-name').val('');
-        $('#variable-text-value').val('');
-        $('#numerical-range-low').val('');
-        $('#numerical-range-high').val('');
-        $('#numerical-range-step').val('');
-
-        // Prevent variable name from being re-used        
-        addedVariableNames.push(variable['name']);
-
-        // Add variable to global list
-        variables.push(variable);
-
-        // Add variable to display
-        document.getElementById('variable-list').innerHTML += '<span class="added-element" type="variable">' + variable['name'] + '</span>';
-    
-        // Add event for deleting variable
-        bindEvents();
+    $('#range-input').on('input', function () {
+        form.updateQuantityValue(this.value);
     });
 
+    $('#add-variable').click(function () {
+        let variable = {
+            'type': $('#variable-dropdown').val(),
+            'name': $('#variable-name').val().toLowerCase().trim(),
+        };
 
-    $('#add-template').click(function () {
-        /*
-        Add template for use during training
-        data generation
-        */
-
-        // Get input data
-        var template = $('#template-input').val().trim() + '\t' + $('#template-target').val().trim();
-
-        // Check that template hasn't already been added
-        if (templates.includes(template)) {
-            alert('Template already exists.');
-            return;
+        // Parse input values
+        if (variable.type == 'text') {
+            variable.values = inputParser.getTextValues();
+        } else {
+            variable.values = inputParser.getNumericValues();
         }
 
-        // Validate template input
-        if ($('#template-input').val().trim() == '' || $('#template-target').val().trim() == '') {
-            alert('Sentence template and target output required.');
-            return;
-        }
+        // Validate input
+        if (!inputValidator.isValidVariable(variable)) return;
 
-        // Add template to global list
-        templates.push(template);
+        // Display variable
+        $('#variable-list').append(
+            form.constructElement(variable.name, 'variable')
+        );
 
-        // Add template to display
-        document.getElementById('template-list').innerHTML += '<span class="added-element" type="template">' + $('#template-input').val().trim() + '</span>';
+        // Add variable to existing and output lists
+        form.existingVariables.push(variable.name);
+        form.variables.push(variable);
+
+        // Clear input fields and enable deletion
+        form.clearVariableFields();
+        form.bindEvents();
+    });
+
     
-        // Clear input fields
-        $('#template-input').val('');
-        $('#template-target').val('');
+    $('#add-template').click(function () {
+        const input = $('#template-input').val().trim();
+        const target = $('#template-target').val().trim();
+        const template = input + '\t' + target;
 
-        // Add event for deleting template
-        bindEvents();
+        // Validate input
+        if (!inputValidator.isValidTemplate(input, target, template)) return;
+
+        // Display template
+        $('#template-list').append(
+            form.constructElement(input, 'template')
+        );
+
+        // Add template to output list
+        form.templates.push(template);
+
+        // Clear input fields and enable deletion
+        form.clearTemplateFields();
+        form.bindEvents();
     });
 
 
     $('#generate-training-data').click(function () {
-        if (templates.length == 0) {
-            alert('Need at least one template.');
-            return;
-        }
+        // Validate generation inputs
+        if (!inputValidator.isValidGenerator()) return;
 
-        if (quantity == 0) {
-            alert('Need a quantity greater than zero.');
-            return;
-        }
-
-        // Generate data
-        for (var i = 0; i < quantity; i++) {
-            console.log(i);
-            // Pick random sentence template
-            var template = templates[getRandomIndex(templates.length)];
-
-            // Populate template with appropiate variable values
-            var populatedTemplate = '';
-            var generatedVariableValues = {};
-            var templateComponents = template.split('${');
-            for (var j = 0; j < templateComponents.length; j++) {
-                var templateComponent = templateComponents[j].split('}')
-                for (var k = 0; k < templateComponent.length; k++) {
-                    var token = templateComponent[k].toLowerCase().trim();
-                    var variableIndex = addedVariableNames.indexOf(token);
-
-                    // Check whether the token is a variable or not
-                    if (variableIndex != -1) {
-                        if (!(token in generatedVariableValues)) {
-                            // Select random value
-                            var variableValues = variables[variableIndex]['values'];
-                            generatedVariableValues[token] = variableValues[getRandomIndex(variableValues.length)];
-                        }
-                        // Use randomised values consistently for each sentence
-                        populatedTemplate += generatedVariableValues[token];
-                    } else {
-                        // Add regular word token
-                        populatedTemplate += templateComponent[k];
-                    }
-                }
-            }
-            populatedTemplate += '\n';
-    
-            // Add training data
-            trainingData.add(populatedTemplate);
-        }
-
-        // Prepare data for export
-        updateTrainingFileURL();
-        
-        // Export data
+        // Generate and export training data
+        dataGenerator.generate();
+        form.updateBlobUrl();
         document.getElementById('export-training-data').click();
     });
 
-
-    function bindEvents() {
-        /*
-        Enable added elements to
-        be deleted upon selection
-        */
-
-        $('.added-element').click(function () {
-            // Get variable name
-            var elementText = $(this).text();
-            var elementType = $(this).attr('type');
-
-            // Remove element from output list
-            if (elementType == 'variable') {
-                var variableIndex = addedVariableNames.indexOf(elementText);
-                variables.splice(variableIndex, 1);
-                addedVariableNames.splice(variableIndex, 1);
-            } else {
-                // Remove template from output list
-                var templateIndex = templates.indexOf(elementText);
-                templates.splice(templateIndex, 1);
-            }
-
-            // Delete element from display list
-            $(this).remove();
-        });
-    }
 
     // Add tooltips to option headlines
     $('.training-tooltip').simpletooltip({
@@ -227,30 +88,204 @@ $(document).ready(function () {
     });
 });
 
-function updateTextInput(value) {
-    document.getElementById('quantity-range-value').innerText = value;
-    quantity = value;
+const inputParser = {
+    getTextValues() {
+        let outputValues = [];
+        let rawValues = $('#variable-text-value').val().split(',');
+
+        // Tokenise and clean input values
+        for (valueIndex in rawValues) {
+            const value = rawValues[valueIndex].trim();
+            if (value != '') {
+                outputValues.push(value);
+            }
+        }
+        return outputValues;
+    },
+    
+    getNumericValues() {
+        const low = parseInt($('#numerical-range-low').val());
+        const high = parseInt($('#numerical-range-high').val());
+        const step = parseInt($('#numerical-range-step').val());
+
+        // Check that all numerical fields have been populated
+        if (isNaN(low) || isNaN(high) || isNaN(step)) {
+            alert('Numerical range fields cannot be empty.');
+            return;
+        }
+
+        // Check that valid range has been entered
+        if (low > high) {
+            alert('Must enter a valid numerical range.');
+            return;
+        }
+
+        // Generate range values
+        let outputValues = [];
+        for (let value = low; value <= high; value += step) {
+            outputValues.push(value);
+        }
+        return outputValues;
+    },
 }
 
+const inputValidator = {
+    isValidVariable(variable) {
+        // Check that field is not empty
+        if (variable.name == '') {
+            alert('Variable name cannot be empty.');
+            return false;
+        }
 
-function getRandomIndex(max) {
-    return Math.floor(Math.random() * max);
+        // Check that name doesn't already exist
+        if (form.existingVariables.includes(variable.name)) {
+            alert('Variable name already exists!');
+            return false;
+        }
+        
+        // Validate input values
+        if (variable.values.length == 0) {
+            alert('Variable values cannot be empty.');
+            return false;
+        }
+        return true;
+    },
+
+    isValidTemplate(input, target, template) {
+        // Validate template input
+        if (input == '' || target == '') {
+            alert('Sentence template and target output required.');
+            return false;
+        }
+
+        // Check that template hasn't already been added
+        if (form.templates.includes(template)) {
+            alert('Template already exists.');
+            return false;
+        }
+        return true;
+    },
+
+    isValidGenerator() {
+        if (form.templates.length == 0) {
+            alert('Need at least one template.');
+            return false;
+        }
+
+        if (form.quantity == 0) {
+            alert('Need a generation quantity greater than zero.');
+            return false;
+        }
+        return true;
+    },
 }
 
+const form = {
+    quantity: 0,
+    templates: [],
+    variables: [],
+    existingVariables: [],
 
-function updateTrainingFileURL() {
-    var saveButton = document.getElementById('export-training-data');
-    var fileName = 'training-data.txt';
-    var contentType = 'text/plain';
-    var blob = new Blob(trainingData, {type: contentType});
+    clearVariableFields() {
+        $('#variable-name').val('');
+        $('#variable-text-value').val('');
+        $('#numerical-range-low').val('');
+        $('#numerical-range-high').val('');
+        $('#numerical-range-step').val('');
+    },
+    
+    clearTemplateFields() {
+        $('#template-input').val('');
+        $('#template-target').val('');
+    },
+    
+    updateQuantityValue(value) {
+        $('#quantity-range-value').text(value);
+        this.quantity = value;
+    },
 
-    window.URL.revokeObjectURL(saveButton.href);
-    saveButton.href = URL.createObjectURL(blob);
-    saveButton.download = fileName;
+    bindEvents() {
+        // Enable deletion of added elements
+        $('.added-element').click(function () {
+            const text = $(this).text();
+            const type = $(this).attr('type');
+    
+            // Remove from output list
+            if (type == 'variable') {
+                form.variables.splice(form.existingVariables.indexOf(text), 1);
+                form.existingVariables.splice(form.existingVariables.indexOf(text), 1);
+            } else {
+                form.templates.splice(form.templates.indexOf(text), 1);
+            }
+            // Remove from display list
+            $(this).remove();
+        });
+    },
+
+    updateBlobUrl() {
+        let saveButton = document.getElementById('export-training-data');
+        let blob = new Blob(dataGenerator.data, {type: 'text/plain'});
+        window.URL.revokeObjectURL(saveButton.href);
+        saveButton.href = URL.createObjectURL(blob);
+        saveButton.download = 'training-data.txt';
+    },
+
+    constructElement(text, type) {
+        return (
+            '<span class="added-element" type="' + type + '">' +
+                text +
+            '</span>'
+        );
+    },
 }
 
-var addedVariableNames = [];
-var variables = [];
-var templates = [];
-var quantity = 0;
-var trainingData = new Set();
+const dataGenerator = {
+    data: new Set(),
+
+    generate() {
+        const quantity = form.quantity;
+        const templates = form.templates;
+        const variables = form.variables;
+        const existingVariables = form.existingVariables;
+
+        for (let i = 0; i < quantity; i++) {
+            // Pick random sentence template
+            const templateIndex = this.getRandomIndex(templates.length);
+            const template = templates[templateIndex];
+            const templateComponents = template.split('${');
+
+            let dataInstance = '';
+            let generatedValues = {};
+
+            // Populate template with random variable values
+            for (let j = 0; j < templateComponents.length; j++) {
+                const component = templateComponents[j].split('}');
+                
+                for (let k = 0; k < component.length; k++) {
+
+                    const token = component[k].toLowerCase().trim();
+                    const tokenIndex = existingVariables.indexOf(token);
+    
+                    // Check whether the token is a variable
+                    if (tokenIndex != -1) {
+                        if (!(token in generatedValues)) {
+                            const values = variables[tokenIndex].values;
+                            const valueIndex = this.getRandomIndex(values.length);
+                            generatedValues[token] = values[valueIndex];
+                        }
+                        // Ensure consistent values are used for each template
+                        dataInstance += generatedValues[token];
+                    } else {
+                        dataInstance += component[k];
+                    }
+                }
+            }
+            dataInstance += '\n';
+            this.data.add(dataInstance);
+        }
+    },
+
+    getRandomIndex(max) {
+        return Math.floor(Math.random() * max);
+    },
+}
