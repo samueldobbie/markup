5b4b93871286a51c8e54b84b64f5bfe08e082bc4
Samuel Dobbie <samuel.jordan.dobbie@gmail.com>
2021-06-16 22:01:43 +0100

    Add related entity dropdown for config creator

--- tools/static/js/config_creator.js
+++ tools/static/js/config_creator.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
-    $('#add-entity').click(addEntity);
+    $('#add-entity').click(addEntity)
 
-    $('#add-attribute').click(addAttribute);
+    $('#add-attribute').click(addAttribute)
+
+    $("#attribute-relation").change(function() {
+        const optionValue = $(this).val()
+
+        if(optionValue == DEFAULT_OPTION_VALUE) {
+            $(this).addClass("default-option")
+        } else {
+           $(this).removeClass("default-option")
+        }
+    })
 
     // Add tooltips to option headlines
     $('.config-tooltip').simpletooltip({
@@ -10,58 +20,80 @@ $(document).ready(function () {
         color: '#1A1E24',
         background_color: 'white',
         border_width: 4
-    });
-});
+    })
+})
 
 function addEntity() {
-    const entity = $('#entity-name').val().trim();
+    const entity = $('#entity-name').val().trim()
 
     if (isValidEntity(entity)) {
         // Hide empty message and display output list
-        $('#empty-container-message').hide();
-        $('#output-list-container').show();
+        $('#empty-container-message').hide()
+        $('#output-list-container').show()
 
         // Add entity to output list
-        entityList.push(entity + '\n');
+        entityList.push(entity + '\n')
 
-        // Add entity to display list
-        $('<span/>', {
-            'class': 'item',
-            'name': 'entity',
-            'text': entity,
-            'css': {
-                'background-color': colors.pop()
-            }
-        }).appendTo('#entity-list');
-        
-        updateExportUrl();
-        bindEvents();
+        // Update display
+        addEntityToDisplay(entity)
+        addEntityToDropdown(entity)
+
+        updateExportUrl()
+        bindEvents()
     }
 
-    resetInputForms('entity');
+    resetInputForms('entity')
+}
+
+function addEntityToDisplay(entity) {
+    $('<span/>', {
+        'class': 'item',
+        'name': 'entity',
+        'text': entity,
+        'css': {
+            'background-color': colors.pop()
+        }
+    }).appendTo('#entity-list')
+}
+
+function addEntityToDropdown(entity) {
+    $('<option/>', {
+        'value': entity,
+        'text': entity,
+    }).appendTo('#attribute-relation')
 }
 
 function addAttribute() {
-    const name = $('#attribute-name').val().trim();
-    const relation = $('#attribute-relation').val().trim();
-    const dropdown = $('#attribute-dropdown').val().split(',');
+    const name = $('#attribute-name').val().trim()
+    const relation = $('#attribute-relation').val()
+    const dropdown = $('#attribute-dropdown').val().split(',')
 
     // Construct formatted attribute string
-    const attribute = getAttributeString(name, relation, dropdown);
+    const attribute = getAttributeString(name, relation, dropdown)
+
+    if (name == "") {
+        alert('You need to add an attribute name.')
+        return
+    }
+
+    if (dropdown.length == 0 || (dropdown.length == 1 && dropdown[0] == "")) {
+        alert('You need to add at least one dropdown value.')
+        return
+    }
 
     if (attribute) {
         // Validate related entities
         if (entityList.indexOf(relation + '\n') == -1) {
-            alert('You need to add the related entity before using it in an attribute.');
-            return;
+            alert('You need to add the related entity before using it in an attribute.')
+            return
         }
 
         // Hide empty message and display output lists
-        $('#empty-container-message').hide();
-        $('#output-list-container').show();
+        $('#empty-container-message').hide()
+        $('#output-list-container').show()
 
         // Add constructed attribute to output list
-        attributeList.push(attribute + '\n');
+        attributeList.push(attribute + '\n')
 
         // Add attribute to display list
         $('<span/>', {
@@ -71,137 +103,150 @@ function addAttribute() {
             'css': {
                 'background-color': getEntityColor(relation)
             }
-        }).appendTo('#attribute-list');
+        }).appendTo('#attribute-list')
 
-        updateExportUrl();
-        bindEvents();
+        updateExportUrl()
+        bindEvents()
     }
 
-    resetInputForms('attribute');
+    resetInputForms('attribute')
 }
 
 function updateExportUrl() {
-    let saveButton = document.getElementById('save-configuration-file');
-    let blob = new Blob(entityList.concat(attributeList), {type: 'text/plain'});
-    window.URL.revokeObjectURL(saveButton.href);
-    saveButton.href = URL.createObjectURL(blob);
-    saveButton.download = 'annotation.conf';
+    let saveButton = document.getElementById('save-configuration-file')
+    let blob = new Blob(entityList.concat(attributeList), {type: 'text/plain'})
+    window.URL.revokeObjectURL(saveButton.href)
+    saveButton.href = URL.createObjectURL(blob)
+    saveButton.download = 'annotation.conf'
 }
 
 function bindEvents() {
     // Enable element deletion
     $('.item').click(function () {
-        const element = $(this);
-        const elementText = element.text() + '\n';
-        const listType = element.parent().attr('id');
+        const element = $(this)
+        const elementText = element.text() + '\n'
+        const listType = element.parent().attr('id')
 
         // Remove element from output list
         if (listType == 'entity-list') {
-            removeEntity(element, elementText);
+            removeEntity(element, elementText)
         } else {
-            removeAttribute(elementText);
+            removeAttribute(elementText)
         }
 
         // Remove from display and update output
-        removeDisplayedElement(element);
-        updateExportUrl();
-    });
+        removeDisplayedElement(element)
+        updateExportUrl()
+    })
 }
 
 function resetInputForms(type) {
     if (type == 'entity') {
-        $('#entity-name').val('');
+        $('#entity-name').val('')
     } else {
-        $('#attribute-name').val('');
-        $('#attribute-relation').val('');
-        $('#attribute-dropdown').val('');
+        $('#attribute-name').val('')
+        $('#attribute-relation').val('default')
+        $('#attribute-dropdown').val('')
     }
 }
 
 function getAttributeString(name, relation, dropdown) {
-    let attribute = name + ' ' + 'Arg:' + relation + ', Value:';
+    let attribute = name + ' ' + 'Arg:' + relation + ', Value:'
     for (let i = 0; i < dropdown.length; i++) {
         if (i != dropdown.length - 1) {
-            attribute += dropdown[i].trim() + "|";
+            attribute += dropdown[i].trim() + "|"
         } else {
-            attribute += dropdown[i].trim();
+            attribute += dropdown[i].trim()
         }
     }
-    return attribute;
+    return attribute
 }
 
 function isValidEntity(entity) {
-    return entity != '' && !entityList.includes(entity + '\n');
+    return entity != '' && !entityList.includes(entity + '\n')
 }
 
 function removeEntity(element, elementText) {
     // Make entity color available again
-    colors.push(element.css('background-color'));
+    colors.push(element.css('background-color'))
 
-    // Remove from entity list
-    for (let i = 0; i < entityList.length; i++) {
-        if (entityList[i] == elementText) {
-            entityList.splice(i, 1);
-            break;
-        }
-    }
-    // Remove attributes that relate to selected entity
-    removeRelatedAttributes(elementText);
+    // Update display
+    removeEntityFromDisplay(elementText)
+    removeEntityFromDropdown(elementText)
+    removeRelatedAttributes(elementText)
 
     // Remove related attributes from display
     $('span[attribute-for=' + elementText + ']').each(function () {
-        $(this).remove();
-    });
+        $(this).remove()
+    })
+}
+
+function removeEntityFromDisplay(elementText) {
+    for (let i = 0; i < entityList.length; i++) {
+        if (entityList[i] == elementText) {
+            entityList.splice(i, 1)
+            break
+        }
+    }
+}
+
+function removeEntityFromDropdown(elementText) {
+    const option = '#attribute-relation option[value=' + elementText + ']'
+    $(option).remove()
 }
 
 function removeAttribute(elementText) {
     for (let i = 0; i < attributeList.length; i++) {
         if (attributeList[i] == elementText) {
-            attributeList.splice(i, 1);
-            break;
+            attributeList.splice(i, 1)
+            break
         }
     }
 }
 
 function removeRelatedAttributes(elementText) {
-    let index = attributeList.length - 1;
+    let index = attributeList.length - 1
+
     while (index > 0) {
-        const attributeComponent = attributeList[index].split('Arg:')[1];
-        const relatedEntity = attributeComponent.split(', Value:')[0] + '\n';
+        const attributeComponent = attributeList[index].split('Arg:')[1]
+        const relatedEntity = attributeComponent.split(', Value:')[0] + '\n'
         if (relatedEntity == elementText) {
-            attributeList.splice(index, 1);
+            attributeList.splice(index, 1)
         }
-        index--;
+        index--
     }
 }
 
 function removeDisplayedElement(element) {
     // Remove from display
-    element.remove();
+    element.remove()
 
     // Show message if list is empty
     if (entityList.length == 1 && attributeList.length == 1) {
-        $('#empty-container-message').show();
-        $('#output-list-container').hide();
+        $('#empty-container-message').show()
+        $('#output-list-container').hide()
     }
 }
 
 function getEntityColor(name) {
-    let color;
+    let color
     $('span[name="entity"]').each(function () {
         if ($(this).text() == name) {
-            color = $(this).css('background-color');
+            color = $(this).css('background-color')
         }
-    });
-    return color;
+    })
+    return color
 }
 
+const DEFAULT_OPTION_VALUE = "default"
+
 // Initalize entity and attribute lists with default headers
-let entityList = ['[entities]\n'];
-let attributeList = ['[attributes]\n'];
+const entityList = ['[entities]\n']
+const attributeList = ['[attributes]\n']
 
 // Colors to be used for entities and attributes in output list
-let colors = getColors(100);
+const colors = getColors(100)
 
 // End session if cookies are disabled
-session.validateCookies();
+session.validateCookies()
+
