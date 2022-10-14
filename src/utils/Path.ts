c2f114b2c944c87a080393d7a47cc9092296efab
Samuel Dobbie <samueldobbie@users.noreply.github.com>
2022-10-14 23:36:33 +0100

    Add enum for page paths

---
+++ src/utils/Path.ts
@@ -0,0 +1,14 @@
+enum Path {
+  Home = "/",
+  SignUp = "/sign-up",
+  SignIn = "/sign-in",
+  ForgotPassword = "/forgot-password",
+  SetupProfile = "/start",
+  Match = "/match",
+  Inbox = "/inbox",
+  Contact = "/contact",
+  Docs = "/docs",
+  NotFound = "*",
+}
+
+export { Path }
