5c7cfc4b6600786832166c850d28354fe5bada2b
Samuel Dobbie <samueldobbie@users.noreply.github.com>
2022-10-18 23:58:18 +0100

    Add modals for creating session and ontology

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,10 @@
-import { createStyles, Card, Text, Container, Grid, Checkbox, Anchor, ActionIcon, Group, Button } from "@mantine/core"
-import { Edit } from "@mui/icons-material"
-import { IconEar, IconEdit, IconPlayerPlay, IconTrash } from "@tabler/icons"
+import { createStyles, Card, Text, Container, Grid, Checkbox, Anchor, ActionIcon, Group, Button, Modal, TextInput, Table } from "@mantine/core"
+import { IconEdit, IconFile, IconPlayerPlay, IconTrash } from "@tabler/icons"
 import { DataTable } from "mantine-datatable"
 import { useState } from "react"
+import { useMantineTheme } from '@mantine/core';
+import { IconUpload, IconX } from '@tabler/icons';
+import { Dropzone } from '@mantine/dropzone';
 
 const useStyles = createStyles((theme) => ({
   card: {
@@ -30,11 +32,14 @@ const useStyles = createStyles((theme) => ({
 }))
 
 function Dashboard() {
+  const theme = useMantineTheme()
+
   const { classes } = useStyles()
 
-  const [tutorialProgress, setTutorialProgress] = useState({
-    readDocs: false,
-  })
+  const [tutorialProgress, setTutorialProgress] = useState({ readDocs: false })
+  const [openNewSessionModal, setOpenNewSessionModal] = useState(false)
+  const [openNewOntologyModal, setOpenNewOntologyModal] = useState(false)
+  const [openExploreOntologyModal, setOpenExploreOntologyModal] = useState(false)
 
   const records = [
     {
@@ -79,127 +84,250 @@ function Dashboard() {
     }
   ]
 
+  const elements = [
+    { ontologyName: 'UMLS', name: 'Carbon' },
+    { ontologyName: 'ICD-10', name: 'Nitrogen' },
+    { ontologyName: 'SNOMED', name: 'Yttrium' },
+  ]
+
+  const rows = elements.map((element) => (
+    <tr key={element.name}>
+      <td>{element.ontologyName}</td>
+      <td><Button compact variant="subtle">Use</Button></td>
+    </tr>
+  ))
+
   return (
-    <Container my="md" size="xl">
-      <Grid>
-        <Grid.Col xs={12}>
-          <Card withBorder radius="md" p="xl" className={classes.card}>
-            <Text size="lg" className={classes.title} weight={500}>
-              Welcome to Markup!
+    <>
+      <Container my="md" size="xl">
+        <Grid>
+          <Grid.Col xs={12}>
+            <Card withBorder radius="md" p="xl" className={classes.card}>
+              <Text size="lg" className={classes.title} weight={500}>
+                Welcome to Markup!
+              </Text>
+
+              <Text size="md" color="dimmed" mt={10} mb="xl">
+                You"re currently on the dashboard where you can manage your annotation
+                sessions and ontologies. Complete the steps below to get familiar with Markup.
+              </Text>
+
+              <Checkbox
+                checked={tutorialProgress.readDocs}
+                label={
+                  <>
+                    Read the <Anchor
+                      href="https://www.notion.so/Markup-Docs-91e9c5cfc6dc416fbcf2241d7c84e6c7"
+                      target="_blank"
+                      sx={{ fontWeight: "bold", textDecoration: "underline" }}
+                      onClick={() => {
+                        setTutorialProgress({
+                          ...tutorialProgress,
+                          readDocs: true,
+                        })
+                      }}
+                    >
+                      docs
+                    </Anchor> (~2 mins)
+                  </>
+                } />
+
+              <Checkbox label="Create an annotation session" mt={15} />
+
+              <Checkbox label="Explore existing ontologies" mt={15} />
+            </Card>
+          </Grid.Col>
+
+          <Grid.Col xs={12} md={6}>
+            <DataTable
+              withBorder
+              highlightOnHover
+              borderRadius="md"
+              records={records}
+              columns={[
+                { accessor: "name", title: <Text size={16}>Annotation Sessions</Text> },
+                {
+                  accessor: "actions",
+                  title: (
+                    <Group spacing={4} position="right" noWrap>
+                      <Button variant="outline" onClick={() => setOpenNewSessionModal(true)}>
+                        Create Session
+                      </Button>
+                    </Group>
+                  ),
+                  textAlignment: "right",
+                  render: (company) => (
+                    <Group spacing={4} position="right" noWrap>
+                      <ActionIcon color="red">
+                        <IconTrash size={16} />
+                      </ActionIcon>
+
+                      <ActionIcon color="blue">
+                        <IconEdit size={16} />
+                      </ActionIcon>
+
+                      <ActionIcon color="green">
+                        <IconPlayerPlay size={16} />
+                      </ActionIcon>
+                    </Group>
+                  ),
+                },
+              ]}
+            />
+          </Grid.Col>
+
+          <Grid.Col xs={12} md={6}>
+            <DataTable
+              withBorder
+              highlightOnHover
+              borderRadius="md"
+              records={records}
+              columns={[
+                { accessor: "name", title: <Text size={16}>Ontology</Text> },
+                {
+                  accessor: "actions",
+                  title: (
+                    <Group spacing={4} position="right" noWrap>
+                      <Button variant="subtle" onClick={() => setOpenExploreOntologyModal(true)}>
+                        Explore
+                      </Button>
+
+                      <Button variant="outline" onClick={() => setOpenNewOntologyModal(true)}>
+                        Import Ontology
+                      </Button>
+                    </Group>
+                  ),
+                  textAlignment: "right",
+                  render: (company) => (
+                    <Group spacing={4} position="right" noWrap>
+                      <ActionIcon color="red">
+                        <IconTrash size={16} />
+                      </ActionIcon>
+
+                      <ActionIcon color="blue">
+                        <IconEdit size={16} />
+                      </ActionIcon>
+
+                      <ActionIcon color="green">
+                        <IconPlayerPlay size={16} />
+                      </ActionIcon>
+                    </Group>
+                  ),
+                },
+              ]}
+            />
+          </Grid.Col>
+        </Grid>
+      </Container>
+
+      <Modal
+        opened={openNewSessionModal}
+        onClose={() => setOpenNewSessionModal(false)}
+        title="Create an annotation session"
+      >
+        <Grid>
+          <Grid.Col>
+            <TextInput
+              required
+              withAsterisk
+              label="Session name"
+              placeholder="Clinical letters"
+            />
+          </Grid.Col>
+
+          <Grid.Col>
+            <Button>
+              Create
+            </Button>
+          </Grid.Col>
+        </Grid>
+      </Modal>
+
+      <Modal
+        opened={openExploreOntologyModal}
+        onClose={() => setOpenExploreOntologyModal(false)}
+        title="Explore existing ontologies"
+      >
+        <Table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </Table>
+      </Modal>
+
+      <Modal
+        opened={openNewOntologyModal}
+        onClose={() => setOpenNewOntologyModal(false)}
+        title="Import an ontology"
+      >
+        <Grid>
+          <Grid.Col>
+            <TextInput
+              required
+              withAsterisk
+              label="Ontology name"
+              placeholder="UMLS"
+            />
+          </Grid.Col>
+
+          <Grid.Col>
+            <Text size={14}>
+              Ontology file
             </Text>
 
-            <Text size="md" color="dimmed" mt={10} mb="xl">
-              You"re currently on the dashboard where you can manage your annotation
-              sessions and ontologies. Complete the steps below to get familiar with Markup.
-            </Text>
+            <Dropzone
+              onDrop={(files) => console.log('accepted files', files)}
+              onReject={(files) => console.log('rejected files', files)}
+              maxSize={3 * 1024 ** 2}
+              accept={[".txt"]}
+              multiple={false}
+            >
+              <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: 'none' }}>
+                <Dropzone.Accept>
+                  <IconUpload
+                    size={50}
+                    stroke={1.5}
+                    color={theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 6]}
+                  />
+                </Dropzone.Accept>
 
-            <Checkbox
-              checked={tutorialProgress.readDocs}
-              label={
-                <>
-                  Read the <Anchor
-                    href="https://www.notion.so/Markup-Docs-91e9c5cfc6dc416fbcf2241d7c84e6c7"
-                    target="_blank"
-                    sx={{ fontWeight: "bold", textDecoration: "underline" }}
-                    onClick={() => {
-                      setTutorialProgress({
-                        ...tutorialProgress,
-                        readDocs: true,
-                      })
-                    }}
-                  >
-                    docs
-                  </Anchor> (~2 mins)
-                </>
-              } />
+                <Dropzone.Reject>
+                  <IconX
+                    size={50}
+                    stroke={1.5}
+                    color={theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]}
+                  />
+                </Dropzone.Reject>
 
-            <Checkbox label="Create an annotation session" mt={15} />
+                <Dropzone.Idle>
+                  <IconFile size={50} stroke={1.5} />
+                </Dropzone.Idle>
 
-            <Checkbox label="Create an ontology" mt={15} />
-          </Card>
-        </Grid.Col>
+                <div>
+                  <Text size="xl" inline>
+                    Drag or click to select ontology file
+                  </Text>
 
-        <Grid.Col xs={12} md={6}>
-          <DataTable
-            withBorder
-            highlightOnHover
-            borderRadius="md"
-            records={records}
-            columns={[
-              { accessor: "name", title: <Text size={16}>Annotation Sessions</Text> },
-              {
-                accessor: "actions",
-                title: (
-                  <Group spacing={4} position="right" noWrap>
-                    <Button variant="outline">
-                      New Session
-                    </Button>
-                  </Group>
-                ),
-                textAlignment: "right",
-                render: (company) => (
-                  <Group spacing={4} position="right" noWrap>
-                    <ActionIcon color="red">
-                      <IconTrash size={16} />
-                    </ActionIcon>
+                  <Text size="sm" color="dimmed" inline mt={7}>
+                    Must be a TXT file. Must not exceed 5MB.
+                  </Text>
+                </div>
+              </Group>
+            </Dropzone>
+          </Grid.Col>
 
-                    <ActionIcon color="blue">
-                      <IconEdit size={16} />
-                    </ActionIcon>
-
-                    <ActionIcon color="green">
-                      <IconPlayerPlay size={16} />
-                    </ActionIcon>
-                  </Group>
-                ),
-              },
-            ]}
-          />
-        </Grid.Col>
-
-        <Grid.Col xs={12} md={6}>
-          <DataTable
-            withBorder
-            highlightOnHover
-            borderRadius="md"
-            records={records}
-            columns={[
-              { accessor: "name", title: <Text size={16}>Ontology</Text> },
-              {
-                accessor: "actions",
-                title: (
-                  <Group spacing={4} position="right" noWrap>
-                    <Button variant="subtle">
-                      Explore
-                    </Button>
-
-                    <Button variant="outline">
-                      New Ontology
-                    </Button>
-                  </Group>
-                ),
-                textAlignment: "right",
-                render: (company) => (
-                  <Group spacing={4} position="right" noWrap>
-                    <ActionIcon color="red">
-                      <IconTrash size={16} />
-                    </ActionIcon>
-
-                    <ActionIcon color="blue">
-                      <IconEdit size={16} />
-                    </ActionIcon>
-
-                    <ActionIcon color="green">
-                      <IconPlayerPlay size={16} />
-                    </ActionIcon>
-                  </Group>
-                ),
-              },
-            ]}
-          />
-        </Grid.Col>
-      </Grid>
-    </Container>
+          <Grid.Col>
+            <Button>
+              Import
+            </Button>
+          </Grid.Col>
+        </Grid>
+      </Modal>
+    </>
   )
 }
 
