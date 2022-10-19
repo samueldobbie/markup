afaa7f374e5c9dd023c07f9974b8b87b4cd2aad2
Samuel Dobbie <samueldobbie@users.noreply.github.com>
2022-10-19 23:07:06 +0100

    Load sessions into data table

--- src/pages/dashboard/SessionTable.tsx
+++ src/pages/dashboard/SessionTable.tsx
@@ -1,8 +1,9 @@
 import { Group, Button, ActionIcon, Text, Grid, Modal, TextInput } from "@mantine/core"
+import { useForm } from "@mantine/form"
 import { IconTrash, IconEdit, IconPlayerPlay } from "@tabler/icons"
 import { DataTable } from "mantine-datatable"
-import { useState } from "react"
-import { addAnnotationSession } from "utils/Database"
+import { useEffect, useState } from "react"
+import { database, Session } from "utils/Database"
 import { ModalProps } from "./Interfaces"
 
 interface Props {
@@ -11,49 +12,13 @@ interface Props {
 
 function SessionTable({ completeTutorialStep }: Props) {
   const [openedModal, setOpenedModal] = useState(false)
+  const [sessions, setSessions] = useState<Session[]>([])
 
-  const records = [
-    {
-      "id": "ab1e3aa6-3116-4e0d-a33d-9262aac86747",
-      "name": "Pfeffer and Sons",
-    },
-    {
-      "id": "6c2c52f1-e197-4892-ae8e-5b5e42c226cb",
-      "name": "Hettinger, Willms and Connelly",
-    },
-    {
-      "id": "9a2e51e0-5bbe-49af-a748-546509792e28",
-      "name": "Champlin - Spencer",
-    },
-    {
-      "id": "41e6105b-1115-4414-aaa6-ace1944ab3f2",
-      "name": "Zulauf, McLaughlin and Jaskolski",
-    },
-    {
-      "id": "dcc6476c-2b6c-4acd-955f-32a0337b5832",
-      "name": "Shanahan - Turcotte",
-    },
-    {
-      "id": "ccdbb85d-2175-4865-a69c-a76557216364",
-      "name": "Gutkowski Inc",
-    },
-    {
-      "id": "19df3e35-1577-48a7-9e2f-f79c4f6c36ef",
-      "name": "Stark Inc",
-    },
-    {
-      "id": "5e50f063-6620-491c-904c-fe8e40488802",
-      "name": "Schmidt and Sons",
-    },
-    {
-      "id": "a46de859-251b-42f6-a6c4-1642beba6b56",
-      "name": "Mohr - Raynor",
-    },
-    {
-      "id": "06f55c10-2481-4b5d-9a70-d8845f5e1381",
-      "name": "Tromp, Runolfsson and Bahringer",
-    }
-  ]
+  useEffect(() => {
+    database
+      .getSessions()
+      .then(setSessions)
+  }, [])
 
   return (
     <>
@@ -61,9 +26,12 @@ function SessionTable({ completeTutorialStep }: Props) {
         withBorder
         highlightOnHover
         borderRadius="md"
-        records={records}
+        records={sessions}
         columns={[
-          { accessor: "name", title: <Text size={16}>Annotation Sessions</Text> },
+          {
+            accessor: "name",
+            title: <Text size={16}>Annotation Sessions</Text>,
+          },
           {
             accessor: "actions",
             title: (
@@ -74,7 +42,7 @@ function SessionTable({ completeTutorialStep }: Props) {
               </Group>
             ),
             textAlignment: "right",
-            render: (company) => (
+            render: (session) => (
               <Group spacing={4} position="right" noWrap>
                 <ActionIcon color="red">
                   <IconTrash size={16} />
@@ -101,31 +69,51 @@ function SessionTable({ completeTutorialStep }: Props) {
   )
 }
 
+interface CreateSessionForm {
+  name: string
+}
+
 function CreateSessionModal({ openedModal, setOpenedModal }: ModalProps) {
+  const form = useForm({
+    initialValues: {
+      name: "",
+    }
+  })
+
+  const handleCreateSession = async (form: CreateSessionForm) => {
+    const { name } = form
+    const error = await database.addSession(name)
+
+    if (error) {
+      alert("Failed to create session")
+    }
+  }
+
   return (
     <Modal
       opened={openedModal}
       onClose={() => setOpenedModal(false)}
       title="Create an annotation session"
     >
-      <Grid>
-        <Grid.Col>
-          <TextInput
-            required
-            withAsterisk
-            label="Session name"
-            placeholder="Clinical letters"
-          />
-        </Grid.Col>
+      <form onSubmit={form.onSubmit((values) => handleCreateSession(values))}>
+        <Grid>
+          <Grid.Col>
+            <TextInput
+              required
+              withAsterisk
+              label="Session name"
+              placeholder="Clinical letters"
+              {...form.getInputProps("name")}
+            />
+          </Grid.Col>
 
-        <Grid.Col>
-          <Button onClick={() => {
-            addAnnotationSession("some-name")
-          }}>
-            Create
-          </Button>
-        </Grid.Col>
-      </Grid>
+          <Grid.Col>
+            <Button type="submit">
+              Create
+            </Button>
+          </Grid.Col>
+        </Grid>
+      </form>
     </Modal>
   )
 }
