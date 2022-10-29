592602480fd90df3db9cb19396bd79677f800bd4
Samuel Dobbie <samueldobbie@users.noreply.github.com>
2022-10-29 22:43:32 +0100

    Generate distinct colours for each entity

--- src/pages/annotate/Config.tsx
+++ src/pages/annotate/Config.tsx
@@ -1,15 +1,19 @@
-import { ActionIcon, Button, Card, Collapse, Group, Radio, Select, Text } from "@mantine/core"
+import { ActionIcon, Button, Card, Collapse, Grid, Group, Radio, Select, Text } from "@mantine/core"
 import { IconChevronDown, IconChevronUp } from "@tabler/icons"
 import { database, WorkspaceConfig } from "pages/database/Database"
 import { useState, useEffect } from "react"
+import { useRecoilState } from "recoil"
+import { activeEntityState, entityColoursState } from "store/Annotate"
 import { SectionProps } from "./Interfaces"
 import { Attribute, parseConfig } from "./Parse"
+import distinctColors from "distinct-colors"
 
 function Config({ workspace }: SectionProps) {
   const [config, setConfig] = useState<WorkspaceConfig>()
   const [entities, setEntities] = useState<string[]>([])
   const [attributes, setAttributes] = useState<Attribute[]>([])
-  const [activeEntity, setActiveEntity] = useState("")
+  const [activeEntity, setActiveEntity] = useRecoilState(activeEntityState)
+  const [entityColours, setEntityColours] = useRecoilState(entityColoursState)
   const [activeAttributes, setActiveAttributes] = useState<Attribute[]>([])
   const [entitySectionOpen, setEntitySectionOpen] = useState(true)
   const [attributeSectionOpen, setAttributeSectionOpen] = useState(true)
@@ -31,88 +35,136 @@ function Config({ workspace }: SectionProps) {
   }, [config])
 
   useEffect(() => {
+    if (activeEntity === "") {
+      setActiveAttributes([])
+      return
+    }
+
     setActiveAttributes(attributes.filter(i => (
       i.isGlobal || i.targetEntity === activeEntity
     )))
-
-    console.log(activeEntity)
-    console.log(activeAttributes)
   }, [activeEntity, attributes])
+
+  useEffect(() => {
+    const colours: Record<string, string> = {}
+
+    const palette = distinctColors({
+      count: entities.length,
+      lightMin: 60,
+    })
+
+    entities.forEach((entity, index) => {
+      colours[entity] = palette[index].hex()
+    })
+
+    setEntityColours(colours)
+  }, [entities, setEntityColours])
 
   return (
     <Card withBorder radius="md" p="xl">
-      <Group mb={20}>
-        <Group position="left" noWrap>
-          <Text size={"sm"}>
-            Select an entity
-          </Text>
+      <Grid>
+        <Grid.Col xs={12}>
+          <Title
+            text="Select an entity"
+            open={entitySectionOpen}
+            setOpen={setEntitySectionOpen}
+          />
+        </Grid.Col>
 
-          <ActionIcon onClick={() => setEntitySectionOpen(!entitySectionOpen)}>
-            {entitySectionOpen && <IconChevronDown />}
-            {!entitySectionOpen && <IconChevronUp />}
-          </ActionIcon>
-        </Group>
+        <Grid.Col xs={12}>
+          <Collapse in={entitySectionOpen}>
+            <Group mb={20}>
+              <Radio.Group
+                name="entities"
+                orientation="vertical"
+                onChange={setActiveEntity}
+                spacing="xs"
+              >
+                {entities?.map((entity) => (
+                  <Radio
+                    label={
+                      <span style={{
+                        backgroundColor: entityColours[entity],
+                        padding: 3,
+                        borderRadius: 5,
+                        color: "#333333",
+                      }}>
+                        {entity}
+                      </span>}
+                    value={entity}
+                  />
+                ))}
+              </Radio.Group>
+            </Group>
+          </Collapse>
+        </Grid.Col>
 
-        <Collapse in={entitySectionOpen}>
-          <Group mb={20}>
-            <Radio.Group
-              name="entities"
-              orientation="vertical"
-              onChange={setActiveEntity}
-              spacing="xs"
-            >
-              {entities?.map((entity) => (
-                <Radio
-                  label={entity}
-                  value={entity}
-                />
-              ))}
-            </Radio.Group>
+        <Grid.Col xs={12}>
+          <Title
+            text="Add attributes"
+            open={attributeSectionOpen}
+            setOpen={setAttributeSectionOpen}
+          />
+        </Grid.Col>
+
+        <Grid.Col xs={12}>
+          <Collapse in={attributeSectionOpen}>
+            {activeAttributes.length === 0 &&
+              <Text color="dimmed">
+                Select entity to see attributes
+              </Text>
+            }
+
+            {activeAttributes.length > 0 &&
+              <Group mb={20}>
+                {activeAttributes.map((attribute, index) => (
+                  <Select
+                    data={attribute.options}
+                    placeholder={attribute.name}
+                    size="xs"
+                    key={index + attribute.name}
+                    searchable
+                  />
+                ))}
+              </Group>
+            }
+          </Collapse>
+        </Grid.Col>
+
+        <Grid.Col xs={12}>
+          <Group noWrap>
+            <Button>
+              Add annotation
+            </Button>
+
+            <Button variant="subtle">
+              Export
+            </Button>
           </Group>
-        </Collapse>
-      </Group>
-
-      <Group mb={20}>
-        <Group position="left" noWrap>
-          <Text size={"sm"}>
-            Add attributes
-          </Text>
-
-          <ActionIcon onClick={() => setAttributeSectionOpen(!attributeSectionOpen)}>
-            {attributeSectionOpen && <IconChevronDown />}
-            {!attributeSectionOpen && <IconChevronUp />}
-          </ActionIcon>
-        </Group>
-
-        <Collapse in={attributeSectionOpen}>
-          <Group mb={20}>
-            <Radio.Group
-              name="attributes"
-              orientation="vertical"
-              spacing="xs"
-            >
-              {activeAttributes.map((attribute) => (
-                <Select
-                  data={attribute.options}
-                  placeholder={attribute.name}
-                  size={"xs"}
-                />
-              ))}
-            </Radio.Group>
-          </Group>
-        </Collapse>
-      </Group>
-
-      <Group noWrap>
-        <Button>
-          Add annotation
-        </Button>
-
-        <Button variant="subtle">
-          Export
-        </Button>
-      </Group>
+        </Grid.Col>
+      </Grid>
     </Card>
+  )
+}
+
+interface TitleProps {
+  text: string
+  open: boolean
+  setOpen: (v: boolean) => void
+}
+
+function Title({ text, open, setOpen }: TitleProps) {
+  return (
+    <Group position="left" noWrap>
+      <Text size={"sm"}>
+        {text}
+      </Text>
+
+      <ActionIcon onClick={() => setOpen(!open)}>
+        {open && <IconChevronDown />}
+        {!open && <IconChevronUp />}
+      </ActionIcon>
+    </Group>
   )
 }
 
