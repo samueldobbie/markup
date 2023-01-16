7259bfe8a440c3529d46ed89402c7ee8f110e456
Samuel Dobbie <samueldobbie@users.noreply.github.com>
2023-01-16 20:41:09 +0000

    refactor: move standoff export out of output component

--- src/pages/annotate/Output.tsx
+++ src/pages/annotate/Output.tsx
@@ -5,18 +5,12 @@ import { useRecoilState, useRecoilValue } from "recoil"
 import { database, WorkspaceAnnotation } from "storage/database"
 import { annotationsState, documentIndexState, documentsState, entityColoursState } from "storage/state/Annotate"
 import { SectionProps } from "./Interfaces"
-import JSZip from "jszip"
-import { saveAs } from "file-saver"
 import SmartAssistant from "./SmartAssistant"
 import { DEMO_DOMAINS } from "utils/Demo"
+import { exportStandoffAnnotations } from "./ExportStandoffAnnotations"
 
 type Entity = string
 type AnnotationGroup = Record<Entity, WorkspaceAnnotation[]>
-
-interface AnnotationOutput {
-  name: string
-  payload: string[]
-}
 
 function Output({ workspace }: SectionProps) {
   const entityColours = useRecoilValue(entityColoursState)
@@ -35,79 +29,6 @@ function Output({ workspace }: SectionProps) {
   useEffect(() => {
     setIsDemoSession(DEMO_DOMAINS.map(domain => domain.id).includes(workspace.id))
   }, [workspace.id])
-
-  const exportAnnotations = async () => {
-    const outputs = [] as AnnotationOutput[]
-
-    documents.forEach((doc, index) => {
-      const name = doc.name
-      const output = buildSingleOutput(name, annotations[index])
-
-      outputs.push(output)
-    })
-
-    saveAsZip(outputs)
-  }
-
-  function saveAsZip(outputs: AnnotationOutput[]): void {
-    const zip = new JSZip()
-
-    outputs.forEach((output: AnnotationOutput) => {
-      if (output.name && output.payload) {
-        zip.file(output.name, output.payload.join(""))
-      }
-    })
-
-    zip
-      .generateAsync({ type: "blob" })
-      .then((content) => saveAs(content, "annotations.zip"))
-      .catch(() => console.error("Failed to export annotations. Please try again later."))
-  }
-
-  function buildSingleOutput(name: string, annotations: WorkspaceAnnotation[]): AnnotationOutput {
-    return {
-      name: name.split(".txt")[0] + ".ann",
-      payload: buildExportRows(annotations),
-    }
-  }
-
-  function buildExportRows(annotations: WorkspaceAnnotation[]): string[] {
-    const rows = [] as string[]
-
-    let annotationId = 1
-    let attributeId = 1
-
-    const addAnnotationRow = (annotation: WorkspaceAnnotation) => {
-      const { entity, start_index, end_index, text } = annotation
-      const normalizedText = normalizeText(text)
-      const output = `T${annotationId}\t${entity} ${start_index} ${end_index}\t${normalizedText}\n`
-      rows.push(output)
-    }
-
-    const addAttributeRow = (annotation: WorkspaceAnnotation) => {
-      const { attributes } = annotation
-
-      Object.entries(attributes).forEach(([name, value]) => {
-        const output = `A${attributeId}\t${name} T${annotationId} ${value}\n`
-        rows.push(output)
-        attributeId++
-      })
-    }
-
-    annotations.forEach((annotation: WorkspaceAnnotation) => {
-      addAnnotationRow(annotation)
-      addAttributeRow(annotation)
-      annotationId++
-    })
-
-    return rows
-  }
-
-  function normalizeText(text: string): string {
-    return text
-      .split(/ |\n|\t/)
-      .join("-")
-  }
 
   const deleteAnnotation = (annotationId: string) => {
     database
@@ -175,7 +96,7 @@ function Output({ workspace }: SectionProps) {
                 <Button
                   variant="subtle"
                   leftIcon={<IconDownload size={16} />}
-                  onClick={exportAnnotations}
+                  onClick={() => exportStandoffAnnotations(documents, annotations)}
                 >
                   Export
                 </Button>
