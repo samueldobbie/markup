913d20e66908bb56864fef4ca909a8377ffdbd08
Samuel Dobbie <samueldobbie@users.noreply.github.com>
2023-01-17 21:57:47 +0000

    refactor: make homepage mobile-friendly

--- src/pages/home/SocialProof.tsx
+++ src/pages/home/SocialProof.tsx
@@ -1,6 +1,34 @@
+import { createStyles } from "@mantine/core"
 import { Image, useMantineTheme, Group } from "@mantine/core"
 
+const useStyles = createStyles((theme) => ({
+  nhsImage: {
+    width: "180px !important",
+    marginRight: "250px !important",
+
+    "@media (max-width: 456px)": {
+      width: "110px !important",
+      marginRight: "0px !important",
+      marginBottom: "20px !important",
+    },
+
+    "@media (min-width: 457px) and (max-width: 920px)": {
+      width: "110px !important",
+      marginRight: "0px !important",
+    },
+  },
+
+  sailImage: {
+    width: "450px !important",
+
+    "@media (max-width: 456px)": {
+      width: "300px !important",
+    },
+  },
+}))
+
 function SocialProof() {
+  const { classes } = useStyles()
   const theme = useMantineTheme()
   const logos = theme.colorScheme === "dark"
     ? ["https://i.imgur.com/WrsAhwq.png", "https://i.imgur.com/6CsuX24.png"]
@@ -10,13 +38,12 @@ function SocialProof() {
     <Group position="center" sx={{ opacity: 0.2 }}>
       <Image
         src={logos[0]}
-        width={180}
-        sx={{ marginRight: 250 }}
+        className={classes.nhsImage}
       />
 
       <Image
         src={logos[1]}
-        width={450}
+        className={classes.sailImage}
       />
     </Group>
   )
