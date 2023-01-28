69b22b96b5cbb0a04a46da4baa306ad92250d74e
Samuel Dobbie <samueldobbie@users.noreply.github.com>
2023-01-28 00:29:30 +0000

    feat: enable update of email and password

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,7 +1,19 @@
-import { TextInput, SimpleGrid, Group, Title, Button, Container } from "@mantine/core"
+import { TextInput, SimpleGrid, Group, Title, Button, Container, Alert, Text, Code } from "@mantine/core"
 import { useForm } from "@mantine/form"
+import { IconCheck } from "@tabler/icons"
+import { useEffect, useState } from "react"
+import { supabase } from "utils/Supabase"
+
+interface SettingsForm {
+  email: string
+  password: string
+  passwordConf: string
+}
 
 function Settings() {
+  const [email, setEmail] = useState("")
+  const [accountUpdated, setAccountUpdated] = useState(false)
+
   const form = useForm({
     initialValues: {
       email: "",
@@ -9,54 +21,134 @@ function Settings() {
       passwordConf: "",
     },
     validate: {
-      email: (value) => !/^\S+@\S+$/.test(value),
-      password: (value) => (value.length > 6 ? null : "Must be longer than 6 characters"),
-      passwordConf: (value) => (value.length > 6 ? null : "Must be longer than 6 characters"),
+      email: (value) => (value.length === 0 || /^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      password: (value) => ((value.length === 0 || value.length > 6) ? null : "Must be longer than 6 characters"),
+      passwordConf: (value) => ((value.length === 0 || value.length > 6) ? null : "Must be longer than 6 characters"),
     },
   })
 
+  const handleSubmit = async (values: SettingsForm) => {
+    if (values.password !== values.passwordConf) {
+      form.setErrors({
+        password: "Passwords do not match",
+        passwordConf: "Passwords do not match",
+      })
+
+      return
+    }
+
+    if (values.email.length === 0 && values.password.length === 0) {
+      form.setErrors({
+        email: "Must change at least one field",
+        password: "Must change at least one field",
+        passwordConf: "Must change at least one field",
+      })
+
+      return
+    }
+
+    let hasError = false
+
+    if (values.email.length > 0) {
+      const { error } = await supabase.auth.updateUser({
+        email: values.email,
+      })
+
+      if (error) {
+        form.setErrors({
+          email: error.message,
+        })
+
+        hasError = true
+      }
+    }
+
+    if (values.password.length > 0) {
+      const { error } = await supabase.auth.updateUser({
+        password: values.password,
+      })
+
+      if (error) {
+        form.setErrors({
+          password: error.message,
+          passwordConf: error.message,
+        })
+
+        hasError = true
+      }
+    }
+
+    if (!hasError) {
+      form.reset()
+      setAccountUpdated(true)
+    }
+  }
+
+  useEffect(() => {
+    const f = async () => {
+      const user = await supabase.auth.getUser()
+
+      if (user) {
+        setEmail(user.data.user?.email ?? "")
+      }
+    }
+
+    f()
+  }, [])
+
   return (
     <Container size="sm">
-      <form onSubmit={form.onSubmit(() => { })}>
-        <Title
-          order={2}
-          size="h1"
-          sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}` })}
-          weight={900}
-        >
-          Settings
-        </Title>
+      <Title
+        order={2}
+        size="h1"
+        weight={900}
+      >
+        Settings
+      </Title>
 
+      {accountUpdated && (
+        <Alert icon={<IconCheck size={16} />} title="Updated" color="green" mt="xl">
+          <Text>
+            Your account has been updated.
+          </Text>
+        </Alert>
+      )}
+
+      <Text mt="xl" color="dimmed">
+        Your current email is <Code display="inline" p={5}>{email}</Code>
+      </Text>
+
+      <form onSubmit={form.onSubmit(handleSubmit)}>
         <TextInput
-          label="Update email"
-          placeholder="New email"
-          name="email"
-          variant="filled"
+          label="New email"
+          placeholder="Email"
+          description="Leave blank to keep current email. You will need to verify your new email before you can use it to sign in."
+          type="email"
           mt="xl"
           {...form.getInputProps("email")}
         />
 
         <SimpleGrid cols={2} mt="md" breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
           <TextInput
-            label="Update Password"
-            placeholder="New password"
-            name="email"
-            variant="filled"
+            label="New password"
+            placeholder="Password"
+            description="Leave blank to keep current password."
+            type="password"
             {...form.getInputProps("password")}
           />
 
           <TextInput
-            label="Confirm password"
-            placeholder="Confirm new password"
-            name="email"
-            variant="filled"
+            label="Confirm new password"
+            placeholder="Password"
+            description="Leave blank to keep current password."
+            type="password"
             {...form.getInputProps("passwordConf")}
           />
         </SimpleGrid>
 
         <Group mt="xl">
           <Button type="submit" size="md">
-            Update settings
+            Save changes
           </Button>
         </Group>
       </form>
