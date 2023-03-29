1c8a05ab652527674bcce024ba3785363a222695
Samuel Dobbie <samueldobbie@users.noreply.github.com>
2023-03-29 18:02:58 +0100

    feat: add title component

---
+++ src/components/title/Title.tsx
@@ -0,0 +1,37 @@
+import { Group, ActionIcon, Text } from "@mantine/core"
+import { IconCaretDown, IconCaretRight, IconInfoCircle } from "@tabler/icons"
+
+interface TitleProps {
+  text: string
+  description?: string
+  open: boolean
+  setOpen: (v: boolean) => void
+}
+
+function Title({ text, description, open, setOpen }: TitleProps) {
+  return (
+    <>
+      <Group
+        position="left"
+        onClick={() => setOpen(!open)}
+        sx={{ cursor: "pointer" }}
+        noWrap
+      >
+        <ActionIcon mr={-10}>
+          {open && <IconCaretDown style={{ opacity: 0.8 }} size={18} />}
+          {!open && <IconCaretRight style={{ opacity: 0.8 }} size={18} />}
+        </ActionIcon>
+
+        <Text size="md">
+          {text}
+        </Text>
+
+        <ActionIcon ml={-15}>
+          <IconInfoCircle style={{ opacity: 0.6 }} size={18} />
+        </ActionIcon>
+      </Group>
+    </>
+  )
+}
+
+export default Title
